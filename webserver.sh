342d6d69e97cd53f90a60dce8a49df078075048d
admin <mehov@users.noreply.github.com>
2017-09-13 07:03:11 +0000

    Move the SSH configuration pieces closer together

--- webserver.sh
+++ webserver.sh
@@ -1010,6 +1010,11 @@ if [ "${noroot_Yn}" = "Y" ] ||  [ "${noroot_Yn}" = "y" ]; then
     echo "" > "/home/${SSH_USER}/.ssh/authorized_keys"
     chown -R ${SSH_USER}:www-data $WWW_ROOT
 fi
+# https://www.veeam.com/kb2061
+echo "" >> /etc/ssh/sshd_config
+echo "KexAlgorithms diffie-hellman-group1-sha1,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group14-sha1" >> /etc/ssh/sshd_config
+echo "Ciphers 3des-cbc,blowfish-cbc,aes128-cbc,aes128-ctr,aes256-ctr" >> /etc/ssh/sshd_config
+ssh-keygen -A
 
 
 #/usr/bin/ssh-keygen -A
@@ -1032,12 +1037,6 @@ EOF
 chmod +x /etc/network/if-up.d/iptables 
 
 apt-get -y autoremove
-
-# https://www.veeam.com/kb2061
-echo "" >> /etc/ssh/sshd_config
-echo "KexAlgorithms diffie-hellman-group1-sha1,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group14-sha1" >> /etc/ssh/sshd_config
-echo "Ciphers 3des-cbc,blowfish-cbc,aes128-cbc,aes128-ctr,aes256-ctr" >> /etc/ssh/sshd_config
-ssh-keygen -A
 
 sleep 2
 invoke-rc.d ssh restart
