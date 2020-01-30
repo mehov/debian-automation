mehov/debian-automation

commit 447ca4b88a15435228c7e61226769e4b330c8170

Do not configure nginx for php if it's not installed

spanel/install.sh
@@ -462,9 +462,30 @@ include fastcgi_params;
 EOF
     fi
 
-    PHP_SOCK_PATH=$(grep -iR "\.sock" /etc/php | awk -F "= " '{print $2}')
-    cat > /etc/nginx/snippets/vhost-common.conf << EOF
+    cat > /etc/nginx/snippets/vhost-common.conf << 'EOF'
 index index.php index.html index.htm;
+location = /favicon.ico {
+    log_not_found off;
+    access_log off;
+}
+location = /robots.txt {
+    allow all;
+    log_not_found off;
+    access_log off;
+}
+location ~* \.(ini)$ {
+    return 404;
+}
+location ~ /\.well-known { 
+    allow all;
+}
+location ~ /\. {
+    deny all;
+}
+EOF
+    if [ ! "${PHP_VER}" = "0" ]; then
+        PHP_SOCK_PATH=$(grep -iR "\.sock" /etc/php | awk -F "= " '{print $2}')
+        cat >> /etc/nginx/snippets/vhost-common.conf << EOF
 location ~ \.php {
     if (\$suspicious = 1) {
         access_log /var/log/nginx/suspicious.log suslog;
@@ -475,24 +496,6 @@ location ~ \.php {
     keepalive_timeout 0;
     fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
     fastcgi_pass unix:${PHP_SOCK_PATH};
-}
-location = /favicon.ico {
-    log_not_found off;
-    access_log off;
-}
-location = /robots.txt {
-    allow all;
-    log_not_found off;
-    access_log off;
-}
-location ~* \.(ini)\$ {
-    return 404;
-}
-location ~ /\.well-known { 
-    allow all;
-}
-location ~ /\. {
-    deny all;
 }
 EOF
 

commit ec8fb7e45048a95a1e790a48d067e7b53e4f9c8d

Fix 'Syntax error: } unexpected (expecting fi)' introduced in 447ca4b88a15435228c7e61226769e4b330c8170

spanel/install.sh
@@ -519,6 +519,7 @@ location ~ \.php {
     fastcgi_pass unix:${PHP_SOCK_PATH};
 }
 EOF
+    fi
 
     # perform all letsencrypt validations in a separate directory
     LETSENCRYPT_ROOT="/usr/share/nginx/letsencrypt"